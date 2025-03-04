7fd850149438ab91bc7d79af10dd806e9c4941a8
Naveen Prasad <128902289+naveenprasad07@users.noreply.github.com>
2025-03-04 18:18:41 +0000

    test-push

---
+++ src/modules/studio/ui/components/studio-sidebar/thumbnail-upload-modal.tsx
@@ -0,0 +1,24 @@
+import { ResponsiveModal } from "@/components/responsive-modal";
+import { UploadDropzone } from "@/lib/uploadthing";
+
+interface ThumbnailUploadModalProps {
+  videoId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const ThumbnailUploadModal = ({
+  videoId,
+  open,
+  onOpenChange,
+}: ThumbnailUploadModalProps) => {
+  return (
+    <ResponsiveModal
+      title="Uplaod a thumbnail"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <UploadDropzone endpoint="imageUploader" />
+    </ResponsiveModal>
+  );
+};
